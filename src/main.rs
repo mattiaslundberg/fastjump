50960fac1538c77889f959b526fcbabc2a2d650b
Mattias Lundberg <mattias@kundo.se>
2019-09-04 17:55:34 +0200

    Read cli arguments and output to user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+struct Cli {
+    #[structopt(short, long)]
+    scan: bool,
+
+    pattern: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::from_args();
+
+    println!("{:#?}", cli);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_main() {
+        assert_eq!(1, 1)
+    }
 }
