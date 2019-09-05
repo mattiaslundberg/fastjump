d912eb19eb222304cc570f540490d4e7720290da
Mattias Lundberg <mattias@kundo.se>
2019-09-05 10:36:37 +0200

    Initial config and args handling

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,29 @@ struct Cli {
 }
 
 fn main() {
-    let cli = Cli::from_args();
+    let args = Cli::from_args();
+    let config = get_config_file();
 
-    println!("{:#?}", cli);
+    if args.scan {
+        scan(config, args.pattern);
+    } else {
+        change(config, args.pattern);
+    }
+}
+
+fn get_config_file() -> String {
+    match std::env::var("FASTJUMP_CONFIG") {
+        Ok(val) => val,
+        Err(_e) => String::from("~/.fastjump"),
+    }
+}
+
+fn scan(config: String, pattern: String) {
+    println!("Scanning {}", pattern)
+}
+
+fn change(config: String, pattern: String) {
+    println!("Searching for {}", pattern)
 }
 
 #[cfg(test)]
@@ -20,7 +40,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_main() {
-        assert_eq!(1, 1)
+    fn test_get_config_file() {
+        assert_eq!(get_config_file(), "~/.fastjump");
+        std::env::set_var("FASTJUMP_CONFIG", "/tmp/fastjump_test");
+        assert_eq!(get_config_file(), "/tmp/fastjump_test");
     }
 }
