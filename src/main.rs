514c7991a4f458f5cbec22e65a05f7fcf89bd181
Mattias Lundberg <mattias@kundo.se>
2019-09-07 17:17:58 +0200

    Move scan to custom module

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use fuzzy_matcher::skim::fuzzy_match;
-use regex::Regex;
-use std::collections::VecDeque;
-use std::fs::{self, File, ReadDir};
-use std::io::prelude::*;
+mod scan;
+use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -22,7 +20,7 @@ fn main() {
     let config = Path::new(config_file.as_str());
 
     if args.scan {
-        scan(config, args.pattern);
+        scan::scan(config, args.pattern);
     } else {
         change(config, args.pattern);
     }
@@ -33,39 +31,6 @@ fn get_config_file() -> String {
     match std::env::var("FASTJUMP_CONFIG") {
         Ok(val) => val,
         Err(_e) => String::from(format!("{}/.fastjump", home)),
-    }
-}
-
-fn scan(config: &Path, pattern: String) {
-    let mut file = match File::create(config) {
-        Ok(f) => f,
-        Err(e) => panic!("Could not open file {}", e),
-    };
-
-    let mut queue: VecDeque<String> = VecDeque::new();
-    queue.push_back(pattern);
-
-    while let Some(path_str) = queue.pop_front() {
-        let current_path: &Path = Path::new(path_str.as_str());
-        let dir: ReadDir = match fs::read_dir(current_path) {
-            Ok(dir) => dir,
-            Err(_) => panic!("Failed to open dir {}", path_str),
-        };
-
-        for thing in dir {
-            let path: PathBuf = thing.unwrap().path();
-            let path_string = String::from(path.to_str().unwrap());
-            let is_dotdir: bool = Regex::new(r"/\.").unwrap().is_match(&path_string);
-
-            if path.is_dir() && !is_dotdir {
-                let absolute_path = path.canonicalize().unwrap();
-                let string = absolute_path.to_str().unwrap().as_bytes();
-                file.write(string).unwrap();
-                file.write(b"\n").unwrap();
-
-                queue.push_back(String::from(path.as_path().to_str().unwrap()));
-            }
-        }
     }
 }
 
@@ -110,72 +75,6 @@ mod tests {
         assert!(get_config_file().ends_with(".fastjump"));
         std::env::set_var("FASTJUMP_CONFIG", "/tmp/fastjump_test");
         assert_eq!(get_config_file(), "/tmp/fastjump_test");
-    }
-
-    #[test]
-    #[should_panic(expected = "Could not open ")]
-    fn test_scan_non_existing_dir() {
-        let path = Path::new("/tmp/asdf/");
-        let pattern = String::new();
-        scan(&path, pattern);
-    }
-
-    #[test]
-    fn test_scan_non_recursive_dir() {
-        let path = Path::new("/tmp/scannonrecursive");
-        let pattern = String::from("test_configs/");
-        scan(&path, pattern);
-
-        let file = match File::open(path) {
-            Ok(f) => f,
-            Err(e) => panic!("Could not open file {}", e),
-        };
-        let mut reader = BufReader::new(file);
-        let mut buffer = String::new();
-
-        reader.read_to_string(&mut buffer).unwrap();
-
-        let v: Vec<&str> = buffer.matches("empty").collect();
-        assert_eq!(v, ["empty"]);
-    }
-
-    #[test]
-    fn test_scan_recursive_dir() {
-        let path = Path::new("/tmp/scan_recursive");
-        let pattern = String::from(".");
-        scan(&path, pattern);
-
-        let file = match File::open(path) {
-            Ok(f) => f,
-            Err(e) => panic!("Could not open file {}", e),
-        };
-        let mut reader = BufReader::new(file);
-        let mut buffer = String::new();
-
-        reader.read_to_string(&mut buffer).unwrap();
-
-        let v: Vec<&str> = buffer.matches("empty").collect();
-        assert_eq!(v, ["empty"]);
-    }
-
-    #[test]
-    fn test_scan_skips_dot_dirs() {
-        let path = Path::new("/tmp/scan_recursive");
-        let pattern = String::from(".");
-        scan(&path, pattern);
-
-        let file = match File::open(path) {
-            Ok(f) => f,
-            Err(e) => panic!("Could not open file {}", e),
-        };
-        let mut reader = BufReader::new(file);
-        let mut buffer = String::new();
-
-        reader.read_to_string(&mut buffer).unwrap();
-
-        let v: Vec<&str> = buffer.matches(".git").collect();
-        let e: Vec<&str> = Vec::new();
-        assert_eq!(v, e);
     }
 
     #[test]
