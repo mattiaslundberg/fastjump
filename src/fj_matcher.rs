8cd727e4f797100c50740bbe2f11b7d0d42f5f04
Mattias Lundberg <mattias@kundo.se>
2019-09-13 17:54:42 +0200

    Add initial matcher test

--- src/fj_matcher.rs
+++ src/fj_matcher.rs
@@ -1,6 +1,7 @@
 use fuzzy_matcher::skim::fuzzy_match;
+use std::env;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, SeekFrom};
 
 pub fn matcher(reader: BufReader<File>, pattern: String) -> String {
     let mut best_score = 0;
@@ -30,7 +31,37 @@ pub fn matcher(reader: BufReader<File>, pattern: String) -> String {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs::OpenOptions;
+    use std::io::prelude::*;
+
+    macro_rules! vec_string {
+        ($($x:expr),*) => (vec![$($x.to_string()),*]);
+    }
+
+    fn get_reader(lines: Vec<String>) -> BufReader<File> {
+        let mut dir = env::temp_dir();
+        dir.push("config");
+        let mut file: File = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .read(true)
+            .open(dir)
+            .unwrap();
+
+        for line in lines {
+            file.write(line.as_bytes()).unwrap();
+            file.write(b"\n").unwrap();
+        }
+
+        file.seek(SeekFrom::Start(0)).unwrap();
+        BufReader::new(file)
+    }
 
     #[test]
-    fn test_match_good_match() {}
+    fn test_basic_exact_match() {
+        let lines: Vec<String> = vec_string!["test", "other"];
+        let reader: BufReader<File> = get_reader(lines);
+        let result: String = matcher(reader, String::from("test"));
+        assert_eq!(result, String::from("test"));
+    }
 }
